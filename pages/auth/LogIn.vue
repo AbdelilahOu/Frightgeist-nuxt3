b6247fb552436cb9895ac8a3383a9e5033b6bb67
Abdelilah_OU <gxgamer710@gamil.com>
2022-12-07 07:18:08 -0800

    add needed components for now

--- pages/auth/LogIn.vue
+++ pages/auth/LogIn.vue
@@ -1,6 +1,11 @@
 <template>
-  <main class="w-screen h-screen flex">
-    <section class="w-full h-full">sdjskd</section>
-    <section class="w-full h-full text-black">sldksmlsdl</section>
+  <main
+    class="w-screen h-screen items-center justify-center lg:grid grid-cols-[1fr_30px_1fr]"
+  >
+    <section class="w-full hidden lg:flex h-full bg-white">sdjskd</section>
+    <div class="hidden lg:flex">lkjlksdjfkd</div>
+    <section class="w-full h-full flex items-center justify-center text-black">
+      sldksmlsdl
+    </section>
   </main>
 </template>
