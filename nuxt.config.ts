c22fe6cba9234b3ab4a9e4c6785ff8583945e344
Abdelilah_OU <gxgamer710@gamil.com>
2022-12-08 06:05:32 -0800

    add trpc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["~/assets/tailwind.css"],
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "trpc-nuxt/module"],
   components: true,
+  typescript: {
+    strict: true,
+  },
   runtimeConfig: {},
 });
---
+++ server/trpc/routers/hello.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { router, procedure } from "../index";
+
+export const helloRouter = router({
+  hello: procedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      return {
+        greeting: `hello ${input.text}`,
+      };
+    }),
+});
