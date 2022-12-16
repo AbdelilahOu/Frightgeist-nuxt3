663b9a7f011e9847079eda4703aa4ce057c15a99
Abdelilah_OU <gxgamer710@gmail.com>
2022-12-16 18:44:22 +0000

    cool way to deal with server/middleware

---
+++ server/middleware/auth.ts
@@ -0,0 +1,16 @@
+import UrlPattern from "url-pattern";
+
+export default defineEventHandler(async (event) => {
+  const endPoints = ["/api/vote/:id"];
+
+  const IsHandledByThisMiddleware = endPoints.some((endPoint) => {
+    const pattern = new UrlPattern(endPoint);
+    const { url } = event.req;
+    if (url) {
+      return pattern.match(url);
+    }
+    return false;
+  });
+
+  if (!IsHandledByThisMiddleware) return;
+});
