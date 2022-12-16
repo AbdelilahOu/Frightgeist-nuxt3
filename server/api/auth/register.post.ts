766977fe16f6b0c2eed3fdde4a69601fbe3d4bfd
Abdelilah_OU <gxgamer710@gmail.com>
2022-12-16 18:28:11 +0000

    restructure server folder

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,4 +1,4 @@
-import { createUser } from "../utils/user-repo";
+import { createUser } from "../../utils/user-repo";
 
 export default defineEventHandler(async (event) => {
   const { userName, passWord, email } = await useBody(event);
--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,36 +1,35 @@
-// // additional function helps with process
-// import { genSalt, hash, compare } from "bcrypt";
-// import * as jsonwebtoken from "jsonwebtoken";
-
-// // BCRYPT
-// export const GenerateSalt = async (): Promise<string> => {
-//   return await genSalt();
-// };
-// export const GeneratePassword = async (
-//   password: string,
-//   salt: string
-// ): Promise<string> => {
-//   return await hash(password, salt);
-// };
-// export const ValidatePassword = async (
-//   EnteredPass: string,
-//   SavedPass: string
-// ): Promise<boolean> => {
-//   return await compare(EnteredPass, SavedPass);
-// };
-// // JWT
-// export const CreateToken = async (
-//   id: string,
-//   name: string,
-//   jwt_se: string
-// ): Promise<string> => {
-//   return jsonwebtoken.sign({ id, username: name }, jwt_se);
-// };
-// export const ValidateToken = async (
-//   token: string,
-//   jwt_se: string
-// ): Promise<{ id: string | boolean }> => {
-//   const decoded = jsonwebtoken.verify(token, jwt_se);
-//   console.log(decoded);
-//   return { id: "sfsds" || false };
-// };
+// additional function helps with process
+import { genSalt, hash, compare } from "bcrypt";
+import { sign, verify } from "jsonwebtoken";
+// BCRYPT
+export const GenerateSalt = async (): Promise<string> => {
+  return await genSalt();
+};
+export const GeneratePassword = async (
+  password: string,
+  salt: string
+): Promise<string> => {
+  return await hash(password, salt);
+};
+export const ValidatePassword = async (
+  EnteredPass: string,
+  SavedPass: string
+): Promise<boolean> => {
+  return await compare(EnteredPass, SavedPass);
+};
+// JWT
+export const CreateToken = async (
+  id: string,
+  name: string,
+  jwt_se: string
+): Promise<string> => {
+  return sign({ id, username: name }, jwt_se);
+};
+export const ValidateToken = async (
+  token: string,
+  jwt_se: string
+): Promise<{ id: string | boolean }> => {
+  const decoded = verify(token, jwt_se);
+  console.log(decoded);
+  return { id: "sfsds" || false };
+};
