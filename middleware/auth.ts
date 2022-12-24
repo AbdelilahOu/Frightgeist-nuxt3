8071a64b24abed435930a40d67dece01bb9d84df
Abdelilah_OU <gxgamer710@gmail.com>
2022-12-24 10:51:31 +0000

    add useOurFetch composables to extend headers

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  // if (from.path === "/") {
+  // if (
+  //   !(to.path === "/auth/LogIn" || to.path === "/auth/Register") &&
+  //   !localStorage.getItem("token")
+  // ) {
+  //   if (from.path === "/auth/LogIn" || from.path === "/auth/Register") {
+  //     return navigateTo({ path: "auth/LogIn" });
+  //   }
   //   return navigateTo({ path: "auth/LogIn" });
   // }
 });
