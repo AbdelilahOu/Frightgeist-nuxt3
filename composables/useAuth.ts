b6f88c788a2ae907df0b3f1e235bb8115e426d91
Abdelilah_OU <gxgamer710@gmail.com>
2022-12-25 13:28:28 +0000

    work with localStorage

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,11 +1,21 @@
 export default () => {
   const useAuthToken = () => useState<string>("auth_token");
-  const setAuthToken = (newToken: string) => (useAuthToken().value = newToken);
-  const getAuthToken = (): string => useAuthToken().value;
+  const setAuthToken = (newToken: string) => {
+    useAuthToken().value = newToken;
+    localStorage.setItem("token", newToken);
+  };
+  const getAuthToken = (): string => {
+    return useAuthToken().value ?? localStorage.getItem("token");
+  };
 
   const useAuthUser = () => useState<user>("auth_user");
-  const setAuthUser = (newUser: user) => (useAuthUser().value = newUser);
-  const getAuthUser = (): user => useAuthUser().value;
+  const setAuthUser = (newUser: user) => {
+    useAuthUser().value = newUser;
+    localStorage.setItem("user", JSON.stringify(newUser));
+  };
+  const getAuthUser = (): user => {
+    return useAuthUser().value ?? JSON.parse(localStorage.getItem("user"));
+  };
 
   return {
     getAuthToken,
