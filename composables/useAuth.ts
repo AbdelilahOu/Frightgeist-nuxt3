f190cea192e5a5b89718e0c1e65f4ec859b47f88
Abdelilah_OU <gxgamer710@gmail.com>
2023-01-02 00:09:02 +0000

    main goal is achieved still needs some testing

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,23 +1,18 @@
 import { user } from "~~/types";
 export default () => {
+  //
   const tokenCookie = useCookie<string>("authToken", {
     expires: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
   });
-  const setAuthToken = (newToken: string) => {
-    tokenCookie.value = newToken;
-  };
-  const getAuthToken = (): string => {
-    return tokenCookie.value;
-  };
+  //
   const userCookie = useCookie<user>("authUser", {
     expires: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
   });
-  const setAuthUser = (newUser: user) => {
-    userCookie.value = newUser;
-  };
-  const getAuthUser = (): user => {
-    return userCookie.value;
-  };
+  //
+  const setAuthToken = (newToken: string) => (tokenCookie.value = newToken);
+  const getAuthToken = (): string => tokenCookie.value;
+  const setAuthUser = (newUser: user) => (userCookie.value = newUser);
+  const getAuthUser = (): user => userCookie.value;
   return {
     getAuthToken,
     setAuthToken,
